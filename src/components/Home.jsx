3eba389ee8136a581cd0436297a9c5a9f826e7a1
boomyao <zhangyaoruo@outlook.com>
2018-07-06 16:41:27 +0800

    save

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default () => (
-  <div><h1>Home</h1><Link to='/about'>to about</Link></div>
+  <div><h1>Yao s Home</h1><Link to='/about'>to about</Link></div>
 )
